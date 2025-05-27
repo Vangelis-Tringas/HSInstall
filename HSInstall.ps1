6af73746cf3cafa5ce26a96931e74886c4a651c1
Vangelis-Tringas <213594880+Vangelis-Tringas@users.noreply.github.com>
2025-05-27 12:36:58 +0000

    Create Menu

--- HSInstall.ps1
+++ HSInstall.ps1
@@ -1,1 +1,15 @@
-Test
+function Show-Menu {
+    param(
+        [String]$title = "Main Menu"
+    )
+
+    Clear-Host
+    Write-Host "==============$title================="
+    Write-Host "1) Windows Configuration"
+    Write-Host "2) Softawre Install"
+    Write-Host "3) Set Power Plan"
+    Write-Host "0) Quit"
+    Write-Host "====================================="
+}
+
+Show-Menu
